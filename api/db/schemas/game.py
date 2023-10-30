0c565460e0a3fa9ebf1eb0c68a1fb14d0265ed3c
codeanish <anish@codeanish.com>
2023-10-29 19:42:43 -0700

    Updates to game schema

--- api/db/schemas/game.py
+++ api/db/schemas/game.py
@@ -4,19 +4,17 @@ from pydantic import BaseModel
 
 class GameBase(BaseModel):
     location: str
-
-
-class GameCreate(GameBase):
     date: datetime.datetime
     player_1_id: int
     player_2_id: int
+
+
+class GameCreate(GameBase):
+    pass
 
 
 class Game(GameBase):
     id: int
-    player_1_id: int
-    player_2_id: int
-    date: datetime.datetime
     breaking_player_id: Optional[int]
     winner_id: Optional[int]
 
